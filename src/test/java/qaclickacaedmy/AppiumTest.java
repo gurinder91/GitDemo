87f107f531e7c946548106b6f03e6724b11b13c7
gurinder <gurinder.s@aol.com>
2019-07-17 15:32:43 -0400

    first commit

---
+++ src/test/java/qaclickacaedmy/AppiumTest.java
@@ -0,0 +1,17 @@
+package qaclickacaedmy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void nativeAPPandroid()
+	{
+		System.out.println("nativeAPPandroid");
+	}
+	
+	@Test
+	public void iosAPP()
+	{
+		System.out.println("iosAPP");
+	}
+}
---
+++ src/test/java/qaclickacaedmy/SeleniumTest.java
@@ -0,0 +1,19 @@
+package qaclickacaedmy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+	}
+	
+	@Test
+	public void elementUI()
+	{
+		System.out.println("elementUI");
+	}
+
+}
